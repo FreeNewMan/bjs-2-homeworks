7e6a5718a0f26f97fda1e46ae6334b1b88729fea
FreeNewMan <lutovp@gmail.com>
2024-05-04 11:21:17 +0500

    bug fix 1 task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,24 +2,20 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let d = 0;
-	d = b ** 2 - 4 * a * c;
-	if (d == 0) {
+	let discriminant = 0;
+	discriminant = b ** 2 - 4 * a * c;
+	if (discriminant === 0) {
 		arr.push(-b / (2 * a));
 	}
-	if (d > 0) {
-		arr.push(-b + Math.sqrt(d)) / (2 * a);
-		arr.push(-b - Math.sqrt(d)) / (2 * a);
+	if (discriminant > 0) {
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
 	}
 	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let P = percent / 100 / 12;
-	let S = amount - contribution;
-	let n = countMonths;
-
-	let totalSum = Number(((S * (P + (P / (((1 + P) ** n) - 1)))) * countMonths).toFixed(2));
-	return totalSum;
-
+	let coefficientMonth = percent / 100 / 12;
+	let creditBody = amount - contribution;
+	return Number(((creditBody * (coefficientMonth + (coefficientMonth / (((1 + coefficientMonth) ** countMonths) - 1)))) * countMonths).toFixed(2));
 }
