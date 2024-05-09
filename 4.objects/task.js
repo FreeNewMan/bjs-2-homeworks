8dc87f9c068e727a18ee188e6efb17b6d235f92c
FreeNewMan <lutovp@gmail.com>
2024-05-09 19:51:03 +0500

    4.objects done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.hasOwnProperty('excluded')) {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	let avg = 0;
+	if (this.hasOwnProperty('marks') && (this.marks.length == 0)) {
+		return avg;
+	}
+	if (!this.hasOwnProperty('excluded')) {
+		avg = Number(this.marks.reduce((a, b) => a + b, 0) / this.marks.length);
+	};
+	return avg;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
