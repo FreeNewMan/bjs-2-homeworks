34d6938be7ed9975d45da2a8e0bfdee21d7a77fb
FreeNewMan <lutovp@yandex.ru>
2024-05-22 17:11:32 +0500

    6 exception

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(strParam) {
+	let parsedValue;
+	parsedValue = Number.parseFloat(strParam);
+	if (Number.isNaN(parsedValue)) {
+		throw new Error("Невалидное значение");
+	}
+	return parsedValue;
+}
+
+function validateCount(strParam) {
+	try {
+		return parseCount(strParam);
+	} catch (error) {
+		return error;
+	}
+
+}
+
+class Triangle {
+	constructor(sideA, sideB, sideC) {
+		if ((sideA + sideB) < sideC || (sideA + sideC) < sideB || (sideB + sideC) < sideA) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.sideA = sideA;
+		this.sideB = sideB;
+		this.sideC = sideC;
+	}
+
+	get perimeter() {
+		return this.sideA + this.sideB + this.sideC;
+	}
+
+	get area() {
+		let perimeter = this.perimeter / 2;
+		return Number(Math.sqrt(perimeter * (perimeter - this.sideA) * (perimeter - this.sideB) * (perimeter - this.sideC)).toFixed(3));
+	}
+}
+
+function getTriangle(sideA, sideB, sideC) {
+	try {
+		return new Triangle(sideA, sideB, sideC);
+	} catch (error) {
+		let message = "Ошибка! Треугольник не существует";
+		const obj = Object.freeze({
+			error: error,
+			area: message,
+			perimeter: message
+		});
+		return obj;
+	}
+}
